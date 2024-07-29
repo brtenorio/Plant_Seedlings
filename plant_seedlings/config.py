2edcd5bc2fd77f94d754513a90618cf6ea8e68b5
brtenorio <b.nunes.bruno@gmail.com>
2024-07-29 17:05:01 +0200

    notebook added

---
+++ plant_seedlings/config.py
@@ -0,0 +1,28 @@
+
+# set a random state number
+rs = 42
+
+# set the path for the data base containing the images
+file_path = "/Users/brncat/Downloads/AltaVerde/GitHub/seeds_db/"
+
+
+# Define paths
+main_dir = file_path+'main'
+train_dir = file_path+'train'
+val_dir = file_path+'valid'
+test_dir = file_path+'test'
+
+# Define split ratios
+train_ratio = 0.7
+val_ratio = 0.15
+test_ratio = 0.15
+
+num_classes = 12
+
+image_resize = 224
+
+batch_size_training = 8
+batch_size_validation = 8
+
+num_epochs = 8
+
